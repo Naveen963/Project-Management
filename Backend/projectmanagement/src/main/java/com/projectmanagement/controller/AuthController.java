7884023b3b43795f8150cfb1996f9e4f0f82172a
Naveen Kumar Chitturi <naveenkumar200101@gmail.com>
2025-07-05 01:14:45 +0530

    sign-in router added

---
+++ Backend/projectmanagement/src/main/java/com/projectmanagement/controller/AuthController.java
@@ -0,0 +1,75 @@
+package com.projectmanagement.controller;
+
+import com.projectmanagement.repository.RoleRepository;
+import com.projectmanagement.repository.UserRepository;
+import com.projectmanagement.security.JwtUtils;
+import com.projectmanagement.security.request.LoginRequest;
+import com.projectmanagement.security.response.UserInfoResponse;
+import com.projectmanagement.service.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping(name = "/api/auth")
+public class AuthController {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest request){
+        Authentication authentication;
+        try{
+            authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.getUsername(),
+                            request.getPassword()));
+        }
+        catch (AuthenticationException exception){
+            Map<String,Object> map = new HashMap<>();
+            map.put("message","Bad Credentials");
+            map.put("status",false);
+            return  new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+        }
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetailsImpl userDetails =(UserDetailsImpl) authentication.getPrincipal();
+
+        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item->item.getAuthority())
+                .collect(Collectors.toList());
+
+        UserInfoResponse loginResponse = new UserInfoResponse(userDetails.getId(),userDetails.getUsername(),roles);
+        return  ResponseEntity.ok().header(HttpHeaders.SET_COOKIE,jwtCookie.toString()).body(loginResponse);
+    }
+}
---
+++ Backend/projectmanagement/src/main/java/com/projectmanagement/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.projectmanagement.repository;
+
+import com.projectmanagement.model.AppRole;
+import com.projectmanagement.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRoleName(AppRole appRole);
+}
